db0207b404a2f821c23b1b157d69e93f68ff9f64
morpheous <cerbus.collin@gmail.com>
2018-09-13 19:33:51 -0700

    3

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+const Masto = require( "mastodon" );
+const Reporter = require( "lilreporter" );
+//const RMU = require( "redis-manager-utils" );
+
+const Sleep = require( "./utils/generic.js" ).sleep;
+
+const Personal = require( "./personal.js" );
+
 ( async ()=> {
 
-	await require( "./utils/discord.js" ).initialize();
+	//var MyRedis = new RMU( 2 );
+	//await MyRedis.init();
+
+	var MyDiscord = new Reporter( Personal.reporters );
+	await MyDiscord.init();
+
+	var MyMastodon = new Masto({
+		access_token: Personal.mastodon.token ,
+		timeout_ms: 60*1000 ,
+		api_url: Personal.mastodon.api_url ,
+	});
 
 	process.on( "unhandledRejection" , function( reason , p ) {
 		var xPrps = Object.keys( reason );
@@ -9,7 +27,7 @@
 		console.trace();
 		if ( !reason ) { return; }
 		if ( reason === "Error: read ECONNRESET" ) { require( "./utils/twitter.js" ).reconnect(); }
-		require( "./utils/discord.js" ).postError( reason );
+		MyDiscord.error( reason );
 	});
 
 	process.on( "uncaughtException" , function( err ) {
@@ -17,16 +35,38 @@
 		console.trace();
 		if ( !err ) { return; }
 		const x11 = err.toString();
-		if ( x11 === "Error: read ECONNRESET" ) {
-			setTimeout( function() {
-				// require( "./utils/twitter.js" ).reconnect();
-			} , 3000 );
-		}		
-		require( "./utils/discord.js" ).postError( err );
+		MyDiscord.error( err );
 	});
 	
-	await require( "./utils/generic.js" ).sleep( 2000 );
 	
-	await require( "./utils/twitter.js" ).reconnect();
+	await Sleep( 2000 );
+
+	var latest_id = "";
+	setInterval( async function() {
+
+		var latest = await MyMastodon.get( "timelines/home" , {
+			since_id: latest_id
+		});
+		if ( latest.data ) {
+			console.log( "We Have Data" );
+			//console.log( latest.data );
+			for ( var i = ( latest.data.length - 1 ); i > -1; i-- ) {
+				//console.log( "\nMessage: [ " + i.toString() + " ] === " );
+				if ( latest_id !== latest.data[ i ].id ) { latest_id = latest.data[ i ].id; }
+				//console.log( "latest_id === " + latest_id );
+				//console.log( latest.data[ i ].content );
+				var new_status = latest.data[ i ].content.replace( "<br />" , " " );
+				new_status = new_status.replace( "<br/>" , " " );
+				new_status = new_status.replace( "<br>" , " " );
+				new_status = new_status.replace( /<[^>]+>/g , "" );
+				//console.log( new_status );
+				await MyDiscord.post( new_status );
+				await Sleep( 1000 );
+
+			}
+		}
+		else { console.log( "No Data BibleThump" ); }
+	} , 5000 );
+
 
 })();
